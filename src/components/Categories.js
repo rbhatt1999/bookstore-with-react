2be975bb67333456b2d174fd84e5b3026d75faf3
rbhatt199924 <rbhatt199924@gmail.com>
2023-01-25 10:03:00 +0530

    add git categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,3 +1,23 @@
-const Categories = () => <button type="button">Check status</button>;
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatusAction } from '../redux/categories/categories';
+
+const Categories = () => {
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.categories);
+
+  const checkStatus = () => {
+    dispatch(checkStatusAction());
+  };
+
+  return (
+    <div className="p-2">
+      <span>{categories}</span>
+      <br />
+      <button type="button" onClick={checkStatus}>
+        Check status
+      </button>
+    </div>
+  );
+};
 
 export default Categories;
